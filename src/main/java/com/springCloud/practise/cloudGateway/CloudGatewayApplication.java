fca3a929f33d43e59eace3571aa63a86a1f9af59
satyabhargavaAllu <satyabhargavaa@pyramid.com>
2019-10-13 14:50:20 +0530

    gate way

---
+++ src/main/java/com/springCloud/practise/cloudGateway/CloudGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.springCloud.practise.cloudGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+public class CloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudGatewayApplication.class, args);
+	}
+
+}
