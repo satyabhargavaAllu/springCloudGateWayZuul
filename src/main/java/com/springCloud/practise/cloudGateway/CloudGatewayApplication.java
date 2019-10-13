aed277370f628a8823c2d1cae400c2a4edd4c465
satyabhargavaAllu <satyabhargavaa@pyramid.com>
2019-10-13 15:46:13 +0530

    added filter

--- src/main/java/com/springCloud/practise/cloudGateway/CloudGatewayApplication.java
+++ src/main/java/com/springCloud/practise/cloudGateway/CloudGatewayApplication.java
@@ -4,14 +4,25 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+import com.springClud.practise.filters.AddRequestHeaderFilter;
 
 @SpringBootApplication
 @EnableZuulProxy
 @EnableDiscoveryClient
+@Configuration
 public class CloudGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudGatewayApplication.class, args);
+	}
+	
+	@Bean
+	public AddRequestHeaderFilter addRequestHeaderFilter() {
+		return new AddRequestHeaderFilter();
 	}
 
 }
---
+++ src/main/java/com/springClud/practise/filters/AddRequestHeaderFilter.java
@@ -0,0 +1,33 @@
+package com.springClud.practise.filters;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+public class AddRequestHeaderFilter extends ZuulFilter {
+
+	@Override
+	public boolean shouldFilter() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+		RequestContext ctx = RequestContext.getCurrentContext();
+		ctx.addZuulRequestHeader("x-location","USA");
+		return null;
+	}
+
+	@Override
+	public String filterType() {
+		return "pre";
+	}
+
+	@Override
+	public int filterOrder() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
